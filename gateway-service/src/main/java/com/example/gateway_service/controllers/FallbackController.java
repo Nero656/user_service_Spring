fc29a74357f54fb903fa293a9db951385d207a18
Nero656 <anotonbiserov@gmail.com>
2025-12-19 15:17:25 +0300

    убрал лишние параметры докер компоузе, сделал проверку для всех методов эндпоинтов user-service

--- gateway-service/src/main/java/com/example/gateway_service/controllers/FallbackController.java
+++ gateway-service/src/main/java/com/example/gateway_service/controllers/FallbackController.java
@@ -1,14 +1,31 @@
 package com.example.gateway_service.controllers;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
 @RestController
 public class FallbackController{
     @GetMapping("/fallback/users")
     public ResponseEntity<String> userServiceFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("User Service is temporarily unavailable. Please try again later.");
+    }
+
+    @PostMapping("/fallback/users")
+    public ResponseEntity<String> userPostServiceFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("User Service is temporarily unavailable. Please try again later.");
+    }
+
+    @PatchMapping("/fallback/users")
+    public ResponseEntity<String> userPatchServiceFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("User Service is temporarily unavailable. Please try again later.");
+    }
+
+    @DeleteMapping("/fallback/users")
+    public ResponseEntity<String> userDeleteServiceFallback() {
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
                 .body("User Service is temporarily unavailable. Please try again later.");
     }
